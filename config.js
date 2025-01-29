c00cd37f1961afe420791c2f2ae702434a0c3848
Huaminyo <168801477+Huaminyo@users.noreply.github.com>
2025-01-29 08:05:52 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  gleam: {
+    email: 'your_email@example.com', // Ganti dengan email Gleam Anda
+    password: 'your_password',       // Ganti dengan password Gleam Anda
+    taskUrl: 'https://gleam.io/XXXXX', // Ganti dengan URL tugas Gleam
+  },
+};
